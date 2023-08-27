b098cc05072e255548d0efcab315a25958145c02
Dakiu <dakiu8@hotmail.com>
2023-08-27 00:34:25 -0400

    agregado model

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
+const mongoose = require('mongoose');
 
 app.use(express.static('public'))
+app.use(express.json())
 /*
 app.get('*', (req, res) => {
     res.redirect('/');
 })
 */
+
+
+mongoose.connect('mongodb+srv://admin:9fr2pJfgbNHJ1NVG@cluster0.xbfx86x.mongodb.net/node-api?retryWrites=true&w=majority')
+.then(()=> {
+    console.log('connected to mongoDB')
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`);
+    })
+    
+}).catch(()=>{
+    console.log(error)
+});
+
+/*
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
-
+*/
 app.post('/usuario', async (req, res) =>{
-     console.log(req.body)
-     res.send(req.body);
+    // console.log(req.body)
+    // res.send(req.body);
+ 
+    try{
+     const usuario = await Usuario.create(req.body)
+     res.status(200).json(usuario);
+     res.send('ok')
+ 
+    }catch(error){
+         console.log(error.message);
+         req.statusCode(500).json({message:error.message})
+    }
+ 
  });
-
  app.get('/hola', (req, res) =>{
     res.send('jjj');
 
